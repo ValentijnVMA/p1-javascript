b08482c11ce9d7878770390999687be3bc8f8ff1
ValentijnVMA <9020514@student.zadkine.nl>
2023-09-20 15:40:07 +0200

    opdracht 7.2

--- opdracht 7.1/app.js
+++ opdracht 7.1/app.js
@@ -1,6 +1,8 @@
 var budget = 100
 var product = 60
 
+let productCost = parseInt(prompt("Hoeveel kost het product dat je wilt kopen?"));
+
 if (budget >= product) {
     document.getElementById("result").innerHTML = "U heeft genoeg geld!";
     document.getElementById("result").style.color = "green";
