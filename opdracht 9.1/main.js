f8337a25cb6f44196a855781f53c9a4e640fbb29
ValentijnVMA <9020514@student.zadkine.nl>
2023-09-20 15:59:22 +0200

    opdracht 9.1

---
+++ opdracht 9.1/main.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById("HetModal");
+
+var btn = document.getElementById("Btn");
+
+var span = document.getElementsByClassName("sluit")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
