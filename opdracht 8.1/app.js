26d3f67ef17cce10b9dedbc8e12f406af598a81e
ValentijnVMA <9020514@student.zadkine.nl>
2023-09-20 15:49:23 +0200

    opdracht 8.2

--- opdracht 8.1/app.js
+++ opdracht 8.1/app.js
@@ -1,27 +1,47 @@
 function add() {
     let num1 = parseFloat(document.getElementById("input1").value);
     let num2 = parseFloat(document.getElementById("input2").value);
-    let result = num1 + num2;
-    document.getElementById("result").innerHTML = result;
+
+    if (num1 > 0 && num2 > 0) {
+        let result = num1 + num2;
+        document.getElementById("result").innerHTML = result;
+    } else {
+        alert("Het getal is te laag");
+    }
 }
 
 function minus() {
     let num1 = parseFloat(document.getElementById("input1").value);
     let num2 = parseFloat(document.getElementById("input2").value);
-    let result = num1 - num2;
-    document.getElementById("result").innerHTML = result;
+
+    if (num1 > 0 && num2 > 0) {
+        let result = num1 - num2;
+        document.getElementById("result").innerHTML = result;
+    } else {
+        alert("Het getal is te laag");
+    }
 }
 
 function multiply() {
     let num1 = parseFloat(document.getElementById("input1").value);
     let num2 = parseFloat(document.getElementById("input2").value);
-    let result = num1 * num2;
-    document.getElementById("result").innerHTML = result;
+
+    if (num1 > 0 && num2 > 0) {
+        let result = num1 * num2;
+        document.getElementById("result").innerHTML = result;
+    } else {
+        alert("Het getal is te laag");
+    }
 }
 
 function divide() {
     let num1 = parseFloat(document.getElementById("input1").value);
     let num2 = parseFloat(document.getElementById("input2").value);
-    let result = num1 / num2;
-    document.getElementById("result").innerHTML = result;
+
+    if (num1 > 0 && num2 > 0) {
+        let result = num1 / num2;
+        document.getElementById("result").innerHTML = result;
+    } else {
+        alert("Het getal is te laag");
+    }
 }
